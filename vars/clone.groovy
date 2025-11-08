50bf3a978351fd49036ea4313655ee0e0dfabb33
TanyaRoy1708 <126353763+TanyaRoy1708@users.noreply.github.com>
2025-11-08 20:54:55 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url,String branch,String cred){
    echo 'Cloning the Code'
-   git url:url,branch:master,credentialsId:cred
+   git url:"${url}",branch:"${master}",credentialsId:"${cred}"
    echo "Cloning success!"
 }
